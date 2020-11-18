9361c71844022bb6bc6db80e382b4aff44354f38
김인화 <q901hi@shinsegae.com>
2020-11-18 15:20:45 +0900

    oaj

--- html/oaj/js/ui.js
+++ html/oaj/js/ui.js
@@ -3,8 +3,19 @@ $(function(){
         var currOn = $(this);
             currPath = currOn.attr('data-path');
 
-        currOn.closest('.tab_menu').siblings('div[class^=tab]').hide();
+        currOn.closest('.tab_menu').siblings('div[class^="tab"]').hide();
         currOn.parent('li').addClass('on').siblings().removeClass('on');
         currOn.closest('.tab_menu').siblings('div.'+currPath).show();
+    });
+    $('.aside_menu > li > a').on('click',function(e){
+        $(this).parent('li').addClass('on').siblings().removeClass('on');
+        e.preventDefault();
+    });
+    $('.btn_toggle').on('click',function(){
+        if($(this).hasClass('off')){
+            $(this).removeClass('off').siblings('table').show();
+        }else{
+            $(this).addClass('off').siblings('table').hide();
+        }
     });
 });
