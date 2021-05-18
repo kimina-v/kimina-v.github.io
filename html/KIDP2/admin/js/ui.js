0b6c6d293226e5b59f9a5c5d613c2e430ca101fa
김인화 <q901hi@shinsegae.com>
2021-05-18 09:20:39 +0900

    del

--- html/KIDP2/admin/js/ui.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-    $('.gnb > li > a').mouseover(function(){
-        $(this).addClass('on').parent('li').siblings().children('a').removeClass('on');
-        $('.gnb > li .depth').hide();
-        $('.gnb > li > a.on').siblings('.depth').show();
-    });
-});
